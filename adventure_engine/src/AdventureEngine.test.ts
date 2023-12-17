81c05e5273917b8570cd7dee659d319f959da243
dang1412 <dttung1412@gmail.com>
2023-12-17 09:06:56 +0900

    adv_engine: test beast positions

--- adventure_engine/src/AdventureEngine.test.ts
+++ adventure_engine/src/AdventureEngine.test.ts
@@ -2,14 +2,44 @@ import { describe, expect, test } from '@jest/globals'
 import { AdventureEngine } from './AdventureEngine'
 
 describe('AdventureEngine', () => {
-  test('Init state', () => {
+  test('initState state', () => {
     const state = AdventureEngine.initState()
     expect(state.weaponAttrsMap[1]).toEqual({ damage: 1, damageArea: {x: -1, y: -1, w: 3, h: 3} })
+  })
+
+  test('onboardBeast should work', () => {
+    const state = AdventureEngine.initState()
+
+    AdventureEngine.onboardBeast(state, 1, 99, [])
+    expect(state.beastPixelMap[1]).toBe(99)
+    expect(state.pixelBeastMap[99]).toBe(1)
+  })
+
+  test('proceedActions should work', () => {
+    const state = AdventureEngine.initState()
 
     AdventureEngine.onboardBeast(state, 1, 99, [])
     expect(state.beastPixelMap[1]).toBe(99)
 
     const updates = AdventureEngine.proceedActions(state, [{ beastId: 1, pixel: 100 }], [])
     expect(updates.moves).toEqual([{ beastId: 1, pixel: 100 }])
+  })
+
+  test('getAllBeastPositions should work', () => {
+    const state = AdventureEngine.initState()
+
+    AdventureEngine.onboardBeast(state, 1, 99, [])
+    AdventureEngine.onboardBeast(state, 2, 120, [])
+    expect(state.beastPixelMap[1]).toBe(99)
+    expect(state.pixelBeastMap[99]).toBe(1)
+
+    expect(state.beastPixelMap[2]).toBe(120)
+    expect(state.pixelBeastMap[120]).toBe(2)
+
+    const _updates = AdventureEngine.proceedActions(state, [{ beastId: 1, pixel: 100 }], [])
+    AdventureEngine.onboardBeast(state, 3, 99, [])
+
+    const pos = AdventureEngine.getAllBeastPositions(state)
+    expect(pos).toEqual([{ beastId: 1, pixel: 100 }, { beastId: 2, pixel: 120 }, { beastId: 3, pixel: 99 }])
   })
 })
