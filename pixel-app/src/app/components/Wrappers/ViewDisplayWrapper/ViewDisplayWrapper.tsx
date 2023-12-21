d51bffb8d84c9dde5801f5a06def4cb232728980
dang1412 <dttung1412@gmail.com>
2023-12-22 01:32:25 +0900

    app: add featured places

--- pixel-app/src/app/components/Wrappers/ViewDisplayWrapper/ViewDisplayWrapper.tsx
+++ pixel-app/src/app/components/Wrappers/ViewDisplayWrapper/ViewDisplayWrapper.tsx
@@ -13,6 +13,36 @@ import { useNotification } from '@/components/hooks/useNotification'
 import { usePixels } from '@/components/hooks/usePixels'
 import { useImages } from '@/components/hooks/useImages'
 
+const featuredImages: PixelImage[] = [
+  {
+    area: {x: 59, y: 43, w: 3, h: 2},
+    imageUrl: 'https://api.millionpixelland.com/ipfs/QmVH634xKzsVzwSshgjcoAfHG8fzVWWMdhZCRJGDUfzvMy',
+    title: 'Subwallet',
+    subtitle: '100x100 grids NFT Billboard with 2D Metaverse gaming ecosystem',
+    link: 'https://pixelland.io/'
+  },
+  {
+    area: {x: 56, y: 34, w: 8, h: 3},
+    imageUrl: '/images/the-sandbox.jpg',
+    title: 'The Sandbox',
+    subtitle: 'Creators can monetize voxel ASSETS and gaming experiences on the blockchain',
+    link: 'https://www.sandbox.game/en/',
+  },
+  {
+    area: {x: 49, y: 39, w: 6, h: 3},
+    imageUrl: '/images/axie.jpg',
+    title: 'Axie Infinity',
+    subtitle: 'Axie Infinity is a virtual world filled with cute, formidable creatures known as Axies. Axies can be battled, bred, collected, and even used to earn resources & collectibles that can be traded on an open marketplace.',
+    link: 'https://axieinfinity.com/',
+  }
+]
+
+const tabs = ['Featured', 'Owned']
+
+const tabBaseClass = 'inline-block p-4 border-b-2 rounded-t-lg'
+const tabInactive = 'border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300'
+const tabActive = `text-blue-600 border-blue-600 dark:text-blue-500 dark:border-blue-500`
+
 export function ViewDisplayWrapper() {
   const { updateLoading } = useLoading()
   const { notify } = useNotification()
@@ -20,6 +50,9 @@ export function ViewDisplayWrapper() {
   const { pixels, ownedPixels, loadPixels } = usePixels()
 
   const { images, ownedImages, loadImages } = useImages()
+
+  const tabContents = [featuredImages, ownedImages]
+  const [activeIndex, setActiveIndex] = useState(0)
 
   useEffect(() => {
     loadPixels()
@@ -84,7 +117,19 @@ export function ViewDisplayWrapper() {
         upload={upload}
         loadSubImages={loadSubImages}
       />
-      <ViewMarket images={ownedImages}/>
+
+      <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+        <ul className="flex flex-wrap -mb-px">
+
+          {tabs.map((t, i) => (
+            <li className='me-2'>
+              <span className={`${tabBaseClass} ${activeIndex === i ? tabActive : tabInactive}`} onClick={() => setActiveIndex(i)}>{t}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      
+      <ViewMarket images={tabContents[activeIndex]}/>
     </>
   )
 }
